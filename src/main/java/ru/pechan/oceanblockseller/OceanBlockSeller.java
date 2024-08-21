ca89f29ea5b538b2a0badad97f8fdba83627bd52
Fiarr4ik <fiarr4ik@gmail.com>
2024-08-22 02:30:16 +0700

    init commit

---
+++ src/main/java/ru/pechan/oceanblockseller/OceanBlockSeller.java
@@ -0,0 +1,17 @@
+package ru.pechan.oceanblockseller;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OceanBlockSeller extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
